da324a3b32a1e0c67a03909b4e7d6347dd96fda7
Alaneor <rr.rossmann@me.com>
2013-07-07 22:14:59 +0200

    new MethodMapperTrait
    This Trait will be used to map method calls on current object to php's
    functions
    [ci skip]

---
+++ Datatype/Traits/MethodMapperTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Datatype\Traits;
+
+trait MethodMapperTrait
+{
+	abstract protected static function _function_prefix();
+
+	abstract protected function &_get_object_data();
+
+
+	public function __call( $method, $args )
+	{
+		$prefix = static::_function_prefix();
+		$function = $prefix . $method;
+
+		//! is_array( $args ) && $args = [$args];
+
+		if ( function_exists( $function ) )
+		{
+			$data = $this->_get_object_data();
+			$args = array_merge( [&$data], $args );
+
+			return call_user_func_array( $function, $args );
+		}
+		else
+		{
+			$trace = debug_backtrace();
+			trigger_error(
+				'Call to undefined function ' . $function .
+				' in ' . $trace[0]['file'] .
+				' on line ' . $trace[0]['line'],
+				E_USER_ERROR );
+
+			return null;
+		}
+	}
+}

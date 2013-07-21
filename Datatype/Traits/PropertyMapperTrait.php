bc60c99ec02c5f9c8e9f5ea03293442c65e90436
Robert Rossmann <rr.rossmann@me.com>
2013-07-21 22:54:28 +0200

    PropertyMapperTrait - update notice description - the error should not suggest a library-specific usage

--- Datatype/Traits/PropertyMapperTrait.php
+++ Datatype/Traits/PropertyMapperTrait.php
@@ -33,7 +33,7 @@ trait PropertyMapperTrait
 		{
 			$trace = debug_backtrace();
 			trigger_error(
-				'Undefined read-only property: ' . $property .
+				'Undefined property: ' . $property .
 				' in ' . $trace[0]['file'] .
 				' on line ' . $trace[0]['line'],
 				E_USER_NOTICE );
